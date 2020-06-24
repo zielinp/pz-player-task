8e7954af6d401d1701c91c3a81eda5bf5854bf0e
Paulina Zielinska <pzielinska.zielona@gmail.com>
2020-06-24 21:50:58 +0200

    Updates

---
+++ precache-manifest.536d9cabdd0af19951d78daa9cc9e442.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46129794b546549a811ee25aca74441f",
+    "url": "/pz-player-task/index.html"
+  },
+  {
+    "revision": "8d4ec1551629c912a8f1",
+    "url": "/pz-player-task/static/css/2.5f2be707.chunk.css"
+  },
+  {
+    "revision": "170a57357d424e015746",
+    "url": "/pz-player-task/static/css/main.13092a66.chunk.css"
+  },
+  {
+    "revision": "8d4ec1551629c912a8f1",
+    "url": "/pz-player-task/static/js/2.24209b7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pz-player-task/static/js/2.24209b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170a57357d424e015746",
+    "url": "/pz-player-task/static/js/main.4fdff5f9.chunk.js"
+  },
+  {
+    "revision": "22bb548ef1177522c6d9",
+    "url": "/pz-player-task/static/js/runtime-main.d79e1249.js"
+  },
+  {
+    "revision": "d522478d9c40ce1d72360effa1c66521",
+    "url": "/pz-player-task/static/media/Play_active.d522478d.png"
+  },
+  {
+    "revision": "6d8b10090438264aa26fc7631331c427",
+    "url": "/pz-player-task/static/media/back_ico.6d8b1009.svg"
+  },
+  {
+    "revision": "52d51b7d5b834b033266df7aeaa497b9",
+    "url": "/pz-player-task/static/media/bg_image.52d51b7d.jpg"
+  },
+  {
+    "revision": "8e344a799b1e4ac1ec57619e7ae0e4f4",
+    "url": "/pz-player-task/static/media/cover-1.8e344a79.png"
+  },
+  {
+    "revision": "aa246fdc4720067bd23be8489b4e7c8a",
+    "url": "/pz-player-task/static/media/cover.aa246fdc.png"
+  },
+  {
+    "revision": "efd35a48d3758a96543e17f5e6d24850",
+    "url": "/pz-player-task/static/media/hide_ico.efd35a48.svg"
+  },
+  {
+    "revision": "68d542b29051b7d6c682709210fb9042",
+    "url": "/pz-player-task/static/media/more_ico.68d542b2.svg"
+  },
+  {
+    "revision": "908e77adf0762e3ca48d8da50ad268d9",
+    "url": "/pz-player-task/static/media/next_ico.908e77ad.svg"
+  },
+  {
+    "revision": "d560ea63609f1bd760297025444ffa79",
+    "url": "/pz-player-task/static/media/playlist_ico.d560ea63.svg"
+  },
+  {
+    "revision": "61530e1d25eb1ef8ad5d7e15af9ac18a",
+    "url": "/pz-player-task/static/media/previous_ico.61530e1d.svg"
+  },
+  {
+    "revision": "f24e65322df49b5a8fd3c450d993a60d",
+    "url": "/pz-player-task/static/media/repeat_ico.f24e6532.svg"
+  },
+  {
+    "revision": "9cca0f11fe187d5900f978490e200dd4",
+    "url": "/pz-player-task/static/media/shuffle_ico.9cca0f11.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/pz-player-task/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/pz-player-task/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/pz-player-task/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/pz-player-task/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a21957684324528264df44116fc5c48c",
+    "url": "/pz-player-task/static/media/unreleased_cover.a2195768.png"
+  },
+  {
+    "revision": "2bd484f60129b6ae4d7aa799b430a9af",
+    "url": "/pz-player-task/static/media/usertive_logo.2bd484f6.svg"
+  }
+]);
